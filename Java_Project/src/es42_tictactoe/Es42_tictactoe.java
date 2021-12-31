418b8a200fcf594664d3ddfd63b39228d944a7b9
Michael Ibrahim <michael.ibrahim042@gmail.com>
2021-12-31 17:06:39 +0200

    Version 3.1 ... Define the Joystick Controlling

--- Java_Project/src/es42_tictactoe/Es42_tictactoe.java
+++ Java_Project/src/es42_tictactoe/Es42_tictactoe.java
@@ -1,10 +1,13 @@
 package es42_tictactoe;
 
+import animatefx.animation.*;
 import java.net.URL;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.Parent;
@@ -12,59 +15,53 @@ import javafx.scene.image.Image;
 import net.java.games.input.Component;
 import net.java.games.input.Controller;
 import net.java.games.input.ControllerEnvironment;
-import net.java.games.input.Version;
         
 public class Es42_tictactoe extends Application{
     private Scene scene;
     private Parent root;
-    public Controller[] controllersList;
-    public Controller gamepad1;
-    public Controller gamepad2;
-    public Component[] componentsOf1;
-    public Component[] componentsOf2;
-    public IntegerProperty xB1;
-    public IntegerProperty yB1;
-    public IntegerProperty eB1;
-    public IntegerProperty bB1;
-    public IntegerProperty sB1;
-    public IntegerProperty xB2;
-    public IntegerProperty yB2;
-    public IntegerProperty eB2;
-    public IntegerProperty bB2;
-    public IntegerProperty sB2;
+    private Controller[] controllerList = ControllerEnvironment.getDefaultEnvironment().getControllers();
+    private Controller gamepad1;
+    private Controller gamepad2;
+    private static Component[] pad1Components;
+    private static Component[] pad2Components;
+    public static IntegerProperty xJ1Btn = new SimpleIntegerProperty(0);
+    public static IntegerProperty yJ1Btn = new SimpleIntegerProperty(0);
+    public static IntegerProperty selectJ1Btn = new SimpleIntegerProperty(0);
+    public static IntegerProperty backJ1Btn = new SimpleIntegerProperty(0);
+    public static IntegerProperty xJ2Btn = new SimpleIntegerProperty(0);
+    public static IntegerProperty yJ2Btn = new SimpleIntegerProperty(0);
+    public static IntegerProperty selectJ2Btn = new SimpleIntegerProperty(0);
+    public static IntegerProperty backJ2Btn = new SimpleIntegerProperty(0);
     
     @Override
     public void start(Stage primaryStage){
-      
-        controllersList = ControllerEnvironment.getDefaultEnvironment().getControllers();    
-        int tempor = 0;
-        for(int counter = 0; counter < controllersList.length; counter++){
-            if(controllersList[counter].getType().equals(Controller.Type.STICK)){
-               if(tempor == 0){
-                   gamepad1 = controllersList[counter];
-                   tempor++;
-                   System.out.println("Gamepad1 assigned");
-               }//if
-               else{
-                   gamepad2 = controllersList[counter];
-                   System.out.println("Gamepad2 assigned");
-               }//else
+        int assignmentFlag = 0;
+        for(int i = 0; i < controllerList.length; i++){
+            if(controllerList[i].getType() == Controller.Type.STICK){
+                if(assignmentFlag == 0){
+                    gamepad1 = controllerList[i];
+                    pad1Components = gamepad1.getComponents();
+                    assignmentFlag = 1;
+                }//if
+                else{
+                    gamepad2 = controllerList[i];
+                    pad2Components = gamepad2.getComponents();
+                }//else
             }//if
         }//for
-        componentsOf1 = gamepad1.getComponents();
-        componentsOf1 = gamepad1.getComponents();
+        System.out.println(pad1Components[0].getName());// X
+        System.out.println(pad1Components[1].getName());// Y 
+        System.out.println(pad1Components[6].getName());// Btn1 --- O
+        System.out.println(pad1Components[7].getName());// Btn2 --- X
+        System.out.println(pad1Components[8].getName());// Btn3 ---[]
+        System.out.println("----------------------");
+        System.out.println(pad2Components[0].getName());// X 
+        System.out.println(pad2Components[1].getName());// Y 
+        System.out.println(pad2Components[6].getName());// Btn1 --- O
+        System.out.println(pad2Components[7].getName());// Btn2 --- X
+        System.out.println(pad2Components[8].getName());// Btn3 ---[]
         
-        xB1 = new SimpleIntegerProperty(0);
-        yB1 = new SimpleIntegerProperty(0);
-        eB1 = new SimpleIntegerProperty(0);
-        bB1 = new SimpleIntegerProperty(0);
-        sB1 = new SimpleIntegerProperty(0);
-        
-        xB2 = new SimpleIntegerProperty(0);
-        yB2 = new SimpleIntegerProperty(0);
-        eB2 = new SimpleIntegerProperty(0);
-        bB2 = new SimpleIntegerProperty(0);
-        sB2 = new SimpleIntegerProperty(0);
+
         
         Image icon = new Image(Es42_tictactoe.class.getResourceAsStream("ITI.png"));
         try{
